28a6dad094e033bc9889ffd6edc147858ec1277c
thomas <thpouget@microsoft.com>
2019-12-24 12:25:34 -0800

    Implementing fill_pixel_line function

--- src/ppu.c
+++ src/ppu.c
@@ -21,6 +21,7 @@
 #define LCDC_DISPLAY_ENABLED(LCDC_REG)                    (LCDC_REG & 0b10000000) == 1
 
 // STAT register related macros
+
 #define STAT_MODE_2(STAT_REG)              (STAT_REG & 0b00000011) == 0b00000010
 #define STAT_MODE_3(STAT_REG)              (STAT_REG & 0b00000011) == 0b00000011
 #define STAT_MODE_0(STAT_REG)              (STAT_REG & 0b00000011) == 0b00000000
@@ -39,8 +40,25 @@
 #define PPU_MODE_3_CYCLES 172
 #define PPU_MODE_0_CYCLES 204
 #define PPU_MODE_1_CYCLES 4560
-
 #define PPU_FRAME_CYCLES 70224
+
+// LCD related macros
+
+#define SCREEN_PIXEL_HEIGHT 144
+#define SCREEN_PIXEL_WIDTH 160
+#define TILE_PIXEL_HEIGHT 8
+#define TILE_PIXEL_WIDTH 8
+#define BG_TILES_PER_ROW 32
+#define BG_TILES_PER_COLUMN 32
+
+// Functions used in this file
+
+void fill_pixel_line(uint8_t pixel_line[], uint8_t * memory_map);
+uint8_t * get_tile_data_addr(uint8_t cur_x, uint8_t cur_y, uint8_t * memory_map);
+uint8_t get_pixel(uint8_t cur_x, uint8_t cur_y, uint8_t * memory_map);
+uint8_t get_tile_pixel(uint8_t tile_offset_x, uint8_t tile_offset_y, uint8_t * tile_data_addr);
+void synchronize_clocks(GameBoy * gameboy_ptr, Ppu * ppu_ptr);
+void change_stat_mode(Ppu * ppu_ptr, uint8_t * memory_map, uint8_t mode);
 
 Ppu * ppu_init()
 {
@@ -66,7 +84,7 @@ void ppu_step(GameBoy * gameboy_ptr)
     else if (STAT_MODE_3(stat_reg) && ppu_ptr->ppu_cur_mode_clock >= PPU_MODE_3_CYCLES)
     {
         change_stat_mode(ppu_ptr, memory_map, 0);
-        render_scanline(gameboy_ptr);
+        // fill pixel line and send to ui
     }
     else if (STAT_MODE_0(stat_reg) && ppu_ptr->ppu_cur_mode_clock >= PPU_MODE_0_CYCLES)
     {
@@ -85,7 +103,43 @@ void ppu_step(GameBoy * gameboy_ptr)
     }
 }
 
-void render_scanline(GameBoy * gameboy_ptr){}
+void fill_pixel_line(uint8_t pixel_line[], uint8_t * memory_map)
+{
+    uint8_t cur_y = 0; // will be set to scy + ly
+    uint8_t cur_x = 0; // will be set to scx
+
+    for (uint8_t i = 0; i < SCREEN_PIXEL_WIDTH; i++)
+    {
+        pixel_line[i] = get_pixel(cur_x + i, cur_y, memory_map);
+    }
+}
+
+uint8_t * get_tile_data_addr(uint8_t cur_x, uint8_t cur_y, uint8_t * memory_map)
+{
+    // This function will be responsible for checking what tile data storage mode has
+    // been chosen and fetching the correct tile data address
+
+    uint8_t * bg_tilemap = memory_map + 0x9800;
+    uint8_t * bg_tileset = memory_map + 0x9000;
+    uint8_t tile_line = (uint8_t) (cur_y /  TILE_PIXEL_HEIGHT);
+    uint8_t tile_column = (uint8_t) (cur_x / TILE_PIXEL_WIDTH);
+    return bg_tileset + bg_tilemap[BG_TILES_PER_ROW * tile_line + tile_column];
+}
+
+uint8_t get_tile_pixel(uint8_t tile_offset_x, uint8_t tile_offset_y, uint8_t * tile_data_addr)
+{
+    // explanation here => https://www.huderlem.com/demos/gameboy2bpp.html
+
+    uint8_t pixel_bit_high = (tile_data_addr[tile_offset_y * 2] & (0b10000000 >> tile_offset_x)) >> (TILE_PIXEL_WIDTH - tile_offset_x);
+    uint8_t pixel_bit_low = (tile_data_addr[tile_offset_y * 2 + 1] & (0b10000000 >> tile_offset_x)) >> (TILE_PIXEL_WIDTH - tile_offset_x);
+    return (pixel_bit_high << 1) | pixel_bit_low;
+}
+
+uint8_t get_pixel(uint8_t cur_x, uint8_t cur_y, uint8_t * memory_map)
+{
+    uint8_t * tile_data_addr = get_tile_data_addr(cur_x, cur_y, memory_map);
+    return get_tile_pixel(cur_x % TILE_PIXEL_WIDTH, cur_y % TILE_PIXEL_HEIGHT, tile_data_addr);
+}
 
 void synchronize_clocks(GameBoy * gameboy_ptr, Ppu * ppu_ptr)
 {
